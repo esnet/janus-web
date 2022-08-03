8df1dd1d0c16fe445e42e740374375edc380f13f
Md Arifuzzaman <arif@nevada.unr.edu>
2022-08-03 02:28:16 -0700

    create new session

--- webapp/session_manager/urls.py
+++ webapp/session_manager/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('start/<int:session_id>/', views.start_session, name='start_session'),
     path('stop/<int:session_id>/', views.stop_session, name='stop_session'),
     path('delete/<int:session_id>/', views.delete_session, name='delete_session'),
+    path('create/', views.create_session, name='create_session'),
     path('<int:session_id>/', views.view_session, name='view_session'),
     path('', views.list_sessions, name='list_sessions'),
 ]
