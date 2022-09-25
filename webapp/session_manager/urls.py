ca81d06eda905ef1bc0cc6a88978a3c7db2e9bc7
Md Arifuzzaman <arif@nevada.unr.edu>
2022-09-24 23:26:18 -0700

    implemented profile management

--- webapp/session_manager/urls.py
+++ webapp/session_manager/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:session_id>/', views.view_session, name='view_session'),
     path('', views.list_sessions, name='list_sessions'),
     path('profiles/', views.list_profiles, name='list_profiles'),
+    path('profiles/create/', views.create_profile, name='create_profile'),
     path('profiles/delete/<str:pname>/', views.delete_profile, name='delete_profile'),
     path('nodes/', views.list_nodes, name='list_nodes'),
 ]
