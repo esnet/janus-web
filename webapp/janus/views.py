707fc9b0f2b1eb66ce3c70a59dcce34af6dcd6d5
Md Arifuzzaman <arif@nevada.unr.edu>
2022-10-25 13:03:09 -0700

    basic authentications and user access control

--- webapp/janus/views.py
+++ webapp/janus/views.py
@@ -1,5 +1,6 @@
 import logging
 from . import services
+from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponseServerError, HttpResponseNotFound
 from django.urls import reverse
@@ -7,36 +8,42 @@ from django.urls import reverse
 
 logger = logging.getLogger(__name__)
 
-def list_sessions_by_user(request):
-    if "name" in request.session:
-        name = request.user
-        login = True
-        sessions = services.get_sessions_by_user(name)
+# def list_sessions_by_user(request):
+#     if request.user.is_authenticated:
+#         name = request.user
+#         login = True
+#         sessions = services.get_sessions_by_user(name)
 
-        content = {
-            "name": name,
-            "login": login,
-            "sessions": sessions
-        }
+#         content = {
+#             "name": name,
+#             "login": login,
+#             "sessions": sessions
+#         }
 
-        return render(request, 'home.html', content)
-    else:
-        return HttpResponseRedirect('/login/')
+#         return render(request, 'home.html', content)
+#     else:
+#         return HttpResponseRedirect('/')
 
 
 def list_sessions(request):
-    status, sessions = services.get_session_info()
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        name = None
+    else:
+        name = request.user.username
+
+    status, sessions = services.get_session_info(name=name)
     login = request.user.is_authenticated
     content = {
-        'login': login
+        'login': login,
+        'is_admin': is_admin,
     }
 
     if status:
-        if login:
-            content['sessions'] = sessions
-        else:
-            content['sessions'] = [session for session in sessions if session['profile'] == 'public']
-
+        content['sessions'] = sessions
         logger.info(content)
         return render(request, 'home.html', content)
     else:
@@ -44,86 +51,124 @@ def list_sessions(request):
 
 
 def list_nodes(request):
-    status, nodes = services.get_nodes(verbose=True)
-    if status:
-        content = {
-            'nodes': nodes,
-            'login': request.user.is_authenticated
-        }
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
 
-        return render(request, 'node.html', content)
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        status, nodes = services.get_nodes(verbose=True)
+        if status:
+            content = {
+                'nodes': nodes,
+                'login': request.user.is_authenticated,
+                'is_admin': is_admin
+            }
+
+            return render(request, 'node.html', content)
+        else:
+            return HttpResponseServerError()
     else:
-        return HttpResponseServerError()
+        return HttpResponseRedirect('/')
 
 
 def list_profiles(request):
-    status, profiles = services.get_profiles(verbose=True)
-    # logger.info(profiles)
-    if status:
-        content = {
-            "profiles": profiles,
-            'login': request.user.is_authenticated
-        }
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
 
-        return render(request, 'profile.html', content)
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        status, profiles = services.get_profiles(verbose=True)
+        # logger.info(profiles)
+        if status:
+            content = {
+                "profiles": profiles,
+                'login': request.user.is_authenticated,
+                'is_admin': is_admin
+            }
+
+            return render(request, 'profile.html', content)
+        else:
+            return HttpResponseServerError()
     else:
-        return HttpResponseServerError()
+        return HttpResponseRedirect('/')
 
 
 def view_session(request, session_id):
-    status, sessions = services.get_session_info(session_id=session_id)
-    if status and len(sessions) > 0:
-        for key in sessions[0]:
-            session_id = key
-        content = {
-            "session_d": key,
-            'session': sessions[0][key],
-            'login': request.user.is_authenticated
-        }
+    if request.user.is_authenticated:
+        is_admin = User.objects.get(username=request.user).is_staff
+        if is_admin:
+            name = "admin"
+        else:
+            name = request.user.username
 
-        # logger.debug(content)
-        return render(request, 'session_view.html', content)
+        status, sessions = services.get_session_info(name=name, session_id=session_id)
+        if status and len(sessions) > 0:
+            for key in sessions[0]:
+                session_id = key
+            content = {
+                "session_d": key,
+                'session': sessions[0][key],
+                'login': request.user.is_authenticated,
+                'is_admin': is_admin
+            }
+
+            # logger.debug(content)
+            return render(request, 'session_view.html', content)
+        else:
+            return HttpResponseNotFound()
     else:
-        return HttpResponseNotFound()
+        return HttpResponseRedirect('/')
+
 
 def add_node(request):
-    login = request.user.is_authenticated
-    data = {"errors": list()}
-    if request.method == 'POST':
-        name = request.POST.get('name', None)
-        if name is not None and len(name):
-            data['name'] = name
-        else:
-            data['errors'].append("Invalid name")
-        url = request.POST.get('url', None)
-        if url is not None and len(name):
-            data['url'] = url
-        else:
-            data['errors'].append("Invalid URL")
-        ntype = request.POST.get('ntype', None)
-        if ntype is not None:
-            data['type'] = int(ntype)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
 
-        # XXX use django Forms...
-        if not len(data['errors']):
-            data['kwargs'] = {}
-            status, res = services.add_node(data)
-            if status:
-                return HttpResponseRedirect(reverse('janus:list_nodes'))
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        data = {"errors": list()}
+        if request.method == 'POST':
+            name = request.POST.get('name', None)
+            if name is not None and len(name):
+                data['name'] = name
             else:
-                data['errors'].append(res)
+                data['errors'].append("Invalid name")
+            url = request.POST.get('url', None)
+            if url is not None and len(name):
+                data['url'] = url
+            else:
+                data['errors'].append("Invalid URL")
+            ntype = request.POST.get('ntype', None)
+            if ntype is not None:
+                data['type'] = int(ntype)
 
-    content = {
-        'data': data,
-        'ntypes': services.get_node_types(),
-        'login': request.user.is_authenticated
-    }
-    return render(request, 'add_node.html', content)
+            # XXX use django Forms...
+            if not len(data['errors']):
+                data['kwargs'] = {}
+                status, res = services.add_node(data)
+                if status:
+                    return HttpResponseRedirect(reverse('janus:list_nodes'))
+                else:
+                    data['errors'].append(res)
+
+        content = {
+            'data': data,
+            'ntypes': services.get_node_types(),
+            'login': request.user.is_authenticated,
+            'is_admin': is_admin
+        }
+        return render(request, 'add_node.html', content)
+    else:
+        return HttpResponseRedirect('/')
 
 
 def remove_node(request, nname):
-    if request.user.is_authenticated:
-        status, res = services.remove_node(nname)
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        status, _ = services.remove_node(nname)
         if status:
             return HttpResponseRedirect(reverse('janus:list_nodes'))
         else:
@@ -133,12 +178,14 @@ def remove_node(request, nname):
 
 
 def create_session(request):
-    login = request.user.is_authenticated
-    # if not login:
-    #     return HttpResponseRedirect('/')
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+
+    is_admin = User.objects.get(username=request.user).is_staff
 
     data = {"errors": list()}
     if request.method == 'POST':
+        data["user"] = "admin" if is_admin else request.user.username
         node = request.POST.get('node', None)
         if node is not None:
             data['instances'] = [node]
@@ -169,7 +216,7 @@ def create_session(request):
                 data["errors"].append(res)
 
     _, nodes = services.get_nodes()
-    if login:
+    if is_admin:
         _, profiles = services.get_profiles()
         _, images = services.get_images(nodes[0])
     else:
@@ -181,7 +228,8 @@ def create_session(request):
         'nodes': nodes,
         'profiles': profiles,
         'images': images,
-        'login': request.user.is_authenticated
+        'login': request.user.is_authenticated,
+        'is_admin': is_admin
     }
 
     logger.debug(content)
@@ -192,56 +240,61 @@ def create_profile(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/')
 
-    data = {"errors": list()}
-    if request.method == 'POST':
-        name = request.POST.get('name', None)
-        if not name:
-            data["errors"].append("Invalid name")
-        data['cpu'] = request.POST.get('cpu', 0)
-        if not data['cpu']:
-            data["cpu"] = 0
-        data["cpu"] = int(data["cpu"])
+    is_admin = User.objects.get(username=request.user).is_staff
+    if is_admin:
+        data = {"errors": list()}
+        if request.method == 'POST':
+            name = request.POST.get('name', None)
+            if not name:
+                data["errors"].append("Invalid name")
+            data['cpu'] = request.POST.get('cpu', 0)
+            if not data['cpu']:
+                data["cpu"] = 0
+            data["cpu"] = int(data["cpu"])
 
-        data['mem'] = request.POST.get('memory', 0)
-        if not data['mem']:
-            data['mem'] = 0
-        data['mem'] = int(data['mem'])
+            data['mem'] = request.POST.get('memory', 0)
+            if not data['mem']:
+                data['mem'] = 0
+            data['mem'] = int(data['mem'])
 
-        data['affinity'] = request.POST.get('affinity', "network")
-        data['mgmt_net'] = request.POST.get('mgmt_net', "bridge")
+            data['affinity'] = request.POST.get('affinity', "network")
+            data['mgmt_net'] = request.POST.get('mgmt_net', "bridge")
 
-        data['internal_port'] = request.POST.get('internal_port', None)
-        if not data['internal_port']:
-            data['internal_port'] = None
+            data['internal_port'] = request.POST.get('internal_port', None)
+            if not data['internal_port']:
+                data['internal_port'] = None
 
-        data['internal_port'] = int(data['internal_port']) if data['internal_port'] else None
-        data['qos'] = request.POST.get('qos', None)
-        if not data['qos']:
-            data["qos"] = None
+            data['internal_port'] = int(data['internal_port']) if data['internal_port'] else None
+            data['qos'] = request.POST.get('qos', None)
+            if not data['qos']:
+                data["qos"] = None
 
-        profile = {
-            'name': name,
-            'settings': data
+            profile = {
+                'name': name,
+                'settings': data
+            }
+
+            # XXX use django Forms...
+            if not len(data['errors']):
+                status, res = services.create_profile(profile)
+                if status:
+                    return HttpResponseRedirect(reverse('janus:list_profiles'))
+                else:
+                    data["errors"].append(res)
+
+        _, qos = services.get_qos()
+        content = {
+            'data': data,
+            'qos': qos,
+            'login': request.user.is_authenticated,
+            'is_admin': is_admin
         }
 
-        # XXX use django Forms...
-        if not len(data['errors']):        
-            status, res = services.create_profile(profile)
-            if status:
-                return HttpResponseRedirect(reverse('janus:list_profiles'))
-            else:
-                data["errors"].append(res)
+        logger.debug(content)
+        return render(request, 'create_profile.html', content)
 
-    _, qos = services.get_qos()
-    content = {
-        'data': data,
-        'qos': qos,
-        'login': request.user.is_authenticated
-    }
-
-    logger.debug(content)
-    return render(request, 'create_profile.html', content)
-
+    else:
+        return HttpResponseRedirect('/')
 
 
 def start_session(request, session_id):
