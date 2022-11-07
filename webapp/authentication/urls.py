b49f37e4cdb1a2a6211cec0c6be9529aa0cef9e7
Md Arifuzzaman <arif@nevada.unr.edu>
2022-11-07 01:49:38 -0800

    images, nodes and profiles access control

--- webapp/authentication/urls.py
+++ webapp/authentication/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
+    path('container/', views.image_access_control, name='auth_images'),
+    path('profiles/', views.profile_access_control, name='auth_profiles'),
+    path('nodes/', views.node_access_control, name='auth_nodes'),
 ]
