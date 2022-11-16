b3bde645218b7375225519f4d6f41c41d502a231
Ezra Kissel <kissel@es.net>
2022-11-16 09:37:24 -0500

    Better displays

--- webapp/janus/templatetags/janus_filters.py
+++ webapp/janus/templatetags/janus_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def get_form(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def get_type(value):
+    return type(value).__name__
