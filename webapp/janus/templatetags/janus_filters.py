3a5491067670685193f52ce8adffa8ba5e8567a6
Ezra Kissel <kissel@es.net>
2023-10-26 15:24:51 -0400

    Checkpoint some work in managing network and volume profiles.

--- webapp/janus/templatetags/janus_filters.py
+++ webapp/janus/templatetags/janus_filters.py
@@ -10,3 +10,11 @@ def get_form(dictionary, key):
 @register.filter
 def get_type(value):
     return type(value).__name__
+
+@register.filter
+def get_key(value, arg):
+    return value.get(arg, None)
+
+@register.filter
+def addstr(arg1, arg2):
+    return str(arg1) + str(arg2)
