9ae44f5ef879ee353f18d9008301311b7a306817
Ezra Kissel <kissel@es.net>
2024-05-01 17:19:55 -0400

    Add django extensions to support SSL dev server.

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'crispy_forms',
     'janus',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
