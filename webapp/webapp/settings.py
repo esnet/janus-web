dd4b3ae542e7ea39fb50521cf0fb4161fa61cbc1
Ezra Kissel <kissel@es.net>
2026-06-23 11:37:10 -0400

    Allow control of vite dev mode

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.getenv("JANUS_WEB_SECRET_KEY", key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
-VITE_DEV_MODE = True  # Set to False to use built production assets (npm run build)
+VITE_DEV_MODE = (
+    False if os.getenv("VITE_DEV_MODE", "True").lower() == "false" else True
+)  # Set to False (or env VITE_DEV_MODE=False) to use built production assets (npm run build)
 
 ALLOWED_HOSTS = ["*"]
 
